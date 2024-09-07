8edcce060ec2aeeb825b1a9891192aad02766dc5
Tobi DEGNON <tobidegnon@proton.me>
2024-09-07 08:51:20 +0100

    feat: add support for django >+ 5.1

--- src/falco_toolbox/views.py
+++ src/falco_toolbox/views.py
@@ -9,9 +9,17 @@ from django.views.decorators.http import require_GET
 
 from .conf import app_settings
 
+if django.VERSION[:2] >= (5, 1):
+    from django.contrib.auth.decorators import login_not_required
+else:
+
+    def login_not_required(view_func):
+        return view_func
+
 
 @require_GET
 @cache_control(max_age=app_settings.CACHE_TIME_ROBOTS_TXT, immutable=True, public=True)
+@login_not_required
 def robots_txt(request: HttpRequest) -> HttpResponse:
     return render(request, app_settings.TEMPLATE_ROBOTS_TXT, content_type="text/plain")
 
@@ -20,6 +28,7 @@ def robots_txt(request: HttpRequest) -> HttpResponse:
 @cache_control(
     max_age=app_settings.CACHE_TIME_SECURITY_TXT, immutable=True, public=True
 )
+@login_not_required
 def security_txt(request: HttpRequest) -> HttpResponse:
     return render(
         request,
